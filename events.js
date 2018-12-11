e7a3e2d37ca3a253e2509bf5d00a173c56fa7370
Kumar Abhinav <abhinav0441@gmail.com>
2018-12-11 12:44:41 +0530

    first commit

--- events.js
+++ events.js
@@ -9,26 +9,36 @@ var todos = [
 function displayTodos() {
     var todoListEl = document.getElementById("todo-list");
     todoListEl.innerHTML = '';
-    todos.forEach(todo => {
+    todos.forEach((todo, index) => {
         todoListEl.innerHTML += '<div>' + 
         todo.label + 
         ' ' + 
         (todo.done ? '&#x2713;' : '&#x2717;') + 
+        '<button onclick="deleteTodo('+index+')">delete</button>' +
+        '<button onclick="toggleStatus('+index+')">toggle</button>' +
         '</div>';
     });
 }
 
+function addTodo() {
+    var inputEl = document.getElementById('input');
+    todos.push({label: inputEl.value, done: false });
+    displayTodos();
+    inputEl.value = '';    
+}
+
+function deleteTodo(i) {
+    todos = todos.filter((t, index) => index != i);
+    displayTodos();
+}
+
+function toggleStatus(i) {
+    todos[i].done = !todos[i].done;
+    displayTodos();
+}
+
 window.addEventListener("load", function() {    
     displayTodos();
-    var btn = document.getElementById('btn');
-    btn.addEventListener('click', function() {
-        setTimeout(function() {
-            var inputEl = document.getElementById('input');
-            todos.push({label: inputEl.value, done: false });
-            displayTodos();
-            inputEl.value = '';    
-        }, 2000);
-    });
 });
 
 
